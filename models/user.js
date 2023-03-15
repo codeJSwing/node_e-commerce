578851ed8c11a615f196a5281ec5a9a03625376d
codeJSwing <imlogic20@gmail.com>
2023-03-15 10:33:01 +0900

    check email & incoding password

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const userSchema = mongoose.Schema({
     id: mongoose.Schema.Types.ObjectId,
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,34 @@
 import express from "express";
+import bcrypt from "bcrypt"
 import userModel from "../models/user.js";
 const router = express.Router()
 
 // sign up
 router.post("/signup", async (req, res) => {
     const {email, password, username, birth} = req.body
+    // 1. email 유무 체크
+    // 2. password 암호화 (인코딩)
     try {
+        const user = await userModel.findOne({email})
+        // 1. email 유무 체크
+        if (user){
+            return res.status(404).json({
+                msg: `exists user`
+            })
+        }
+        // 2. password 암호화
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new userModel({
+            email,
+            password: hashedPassword,
+            username,
+            birth
+        })
+        const createUser = await newUser.save()
+        res.json({
+            msg: `successful new User`,
+            user: createUser
+        })
 
     } catch (err) {
         res.status(500).json({
