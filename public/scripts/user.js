8e5cb403aa6dddcb5d4a29929424b630d4472140
codeJSwing <imlogic20@gmail.com>
2023-05-23 16:08:05 +0900

    Refactor: merged seperated js code (in front)

---
+++ public/scripts/user.js
@@ -0,0 +1,85 @@
+const forms = [
+    {
+        form: document.getElementById('registration-form'),
+        url: '/users/registration'
+    },
+    {
+        form: document.getElementById('login-form'),
+        url: '/users/login'
+    },
+    {
+        form: document.getElementById('email-recovery-form'),
+        url: '/users/email-recovery'
+    },
+    {
+        form: document.getElementById('password-recovery-form'),
+        url: '/users/password-recovery'
+    }
+]
+
+forms.forEach((formItem) => {
+    formItem.form?.addEventListener('submit', async (event) => {
+        event.preventDefault()
+
+        const formDataId = formItem.form.id
+
+        let formData
+
+        switch (formDataId) {
+            case 'signup-form' :
+                formData = {
+                    email: document.getElementById('email').value,
+                    name: document.getElementById('name').value,
+                    password: document.getElementById('password').value,
+                    password2: document.getElementById('password2').value,
+                    username: document.getElementById('username').value,
+                    phoneNumber: document.getElementById('phoneNumber').value,
+                    role: document.getElementById('role').value
+                }
+                break
+
+            case 'login-form' :
+                formData = {
+                    email: document.getElementById('email').value,
+                    password: document.getElementById('password').value
+                }
+                break
+
+            case 'email-recovery-form' :
+                formData = {
+                    phoneNumber: document.getElementById('phoneNumber').value,
+                    name: document.getElementById('name').value
+                }
+                break
+
+            case 'password-recovery-form' :
+                formData = {
+                    email: document.getElementById('email').value
+                }
+                break
+        }
+
+        await responseHandler(formItem.url, formData)
+    })
+})
+
+async function responseHandler(url, formData) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+
+    const responseData = await response.json()
+
+    if (response.ok) {
+        alert(responseData.message)
+        if (url !== 'users/login') {
+            window.location.href = '/users/login'
+        }
+    } else {
+        alert(responseData.message)
+    }
+}
